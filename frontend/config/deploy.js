e6ddb8cd4c93c729dcca5d86a257920ce5dcd78f
Sammy Nave <sammy.nave@meyouhealth.com>
2015-05-09 23:34:08 -0400

    deploy setup

---
+++ frontend/config/deploy.js
@@ -0,0 +1,15 @@
+module.exports = {
+  production: {
+    store: {
+      type: "S3",
+      accessKeyId: process.env['S3_KEY'],
+      secretAccessKey: process.env['S3_SECRET'],
+      bucket: "decisionhole.com",
+    },
+    assets: {
+      accessKeyId: process.env['S3_KEY'],
+      secretAccessKey: process.env['S3_SECRET'],
+      bucket: 'decisionhole.com'
+    }
+  }
+};
